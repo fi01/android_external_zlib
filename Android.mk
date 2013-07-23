27660380ae510f510021c64f54ef73e8e0c69bb3
fi01 <furu.is01@gmail.com>
2013-07-23 09:31:55 +0900

    Build only static library for ndk

--- Android.mk
+++ Android.mk
@@ -31,70 +31,11 @@ ifeq ($(ARCH_ARM_HAVE_NEON),true)
 	zlib_arm_flags += -D__ARM_HAVE_NEON
 endif
 
-LOCAL_MODULE := libz
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP $(zlib_arm_flags)
-LOCAL_SRC_FILES := $(zlib_files) $(zlib_arm_files)
-include $(BUILD_SHARED_LIBRARY)
-
 include $(CLEAR_VARS)
 
 LOCAL_ARM_MODE := arm
-LOCAL_MODULE := libz
+LOCAL_MODULE := libz_static
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -O3 -DUSE_MMAP $(zlib_arm_flags)
 LOCAL_SRC_FILES := $(zlib_files) $(zlib_arm_files)
 include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_ARM_MODE := arm
-LOCAL_MODULE := libz
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP
-LOCAL_SRC_FILES := $(zlib_files)
-include $(BUILD_HOST_STATIC_LIBRARY)
-
-
-
-unzip_files := \
-	adler32.c \
-	crc32.c \
-	zutil.c \
-	inflate.c \
-	inftrees.c \
-	inffast.c
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := $(unzip_files)
-LOCAL_MODULE:= libunz
-LOCAL_ARM_MODE := arm
-include $(BUILD_HOST_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := $(unzip_files)
-LOCAL_MODULE:= libunz
-LOCAL_ARM_MODE := arm
-include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:=        \
-	test/minigzip.c
-
-LOCAL_MODULE:= gzip
-
-LOCAL_SHARED_LIBRARIES := libz
-
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:=        \
-	test/minigzip.c
-
-LOCAL_MODULE:= minigzip
-
-LOCAL_STATIC_LIBRARIES := libz
-
-include $(BUILD_HOST_EXECUTABLE)
